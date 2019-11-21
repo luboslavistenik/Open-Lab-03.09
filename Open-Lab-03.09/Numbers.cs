1796b830bc55cfb767e389d8d3bb7455ecc9624f
luboslavistenik <luboslav.istenik@gmail.com>
2019-11-21 09:17:26 +0100

    Done

--- Open-Lab-03.09/Numbers.cs
+++ Open-Lab-03.09/Numbers.cs
@@ -6,7 +6,22 @@ namespace Open_Lab_03._09
     {
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num <= 1)
+                return false;
+
+            if (num == 2)
+                return true;
+
+            if (num % 2 == 0)
+                return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(num));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (num % i == 0)
+                    return false;
+
+            return true;
         }
     }
 }
